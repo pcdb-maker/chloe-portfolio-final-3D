fac0f6c5a4a0b72dbe6cda0cd8dadd75a60b47bf
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-24 10:59:20 +1000

    Updated skills section

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -31,7 +31,7 @@ export const Menu = (props) => {
           <MenuButton label="About" onClick={() => onSectionChange(0)} />
           <MenuButton label="Skills" onClick={() => onSectionChange(1)} />
           <MenuButton label="Projects" onClick={() => onSectionChange(2)} />
-          <MenuButton label="Projects Copy" onClick={() => onSectionChange(3)} />
+          <MenuButton label="Education" onClick={() => onSectionChange(3)} />
           <MenuButton label="Contact" onClick={() => onSectionChange(4)} />
         </div>
       </div>
